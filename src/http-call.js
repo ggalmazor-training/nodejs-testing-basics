e38814b8411efb472477a7c26c65a83780e87cfe
Guille <ggalmazor@gmail.com>
2021-05-12 19:56:26 +0200

    WIP

--- src/http-call.js
+++ src/http-call.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch';
 
 async function greetAndPredictAge(name) {
-  const response = await fetch(`https://api.agify.io?name=${name}`);
+  const response = await fetch(`https://api.agify.io/?name=${name}`);
   const json = await response.json();
   return `Hi, ${name}. I think you're ${json.age} years old`;
 }
--- src/http-call_test.js
+++ src/http-call_test.js
@@ -10,8 +10,15 @@ import {greetAndPredictAge} from "./http-call";
 // https://github.com/nock/nock#install
 // npm install --save-dev nock
 
-test("ads,mads,as.", async () => {
+import nock from 'nock';
+
+test("returns a greeting including the provided name and a guess of the person's age", async () => {
+  const agifyMock = nock('https://api.agify.io')
+    .get('/?name=Guillermo')
+    .reply(200, {age: 42})
+
   const output = await greetAndPredictAge('Guillermo');
 
-  expect(output).toEqual('Hi, Guillermo. I think you\'re 57 years old')
+  expect(output).toEqual('Hi, Guillermo. I think you\'re 42 years old')
+  expect(agifyMock.isDone()).toBe(true);
 })
